ee0905d8525d47410211e47d50028d8ae2956f6a
Shiro Neko <tagiki5@gmail.com>
2020-09-02 16:44:35 -0300

    Envio de Mensagens Completo

--- server.js
+++ server.js
@@ -15,4 +15,18 @@ app.use('/', (req, res) => {
   res.render('index.html');
 });
 
-app.listen(3000);
+let messages = [];
+
+io.on('connection', socket => {
+  console.log(`Socket conectado: ${socket.id}`);
+
+  socket.emit('previousMessages', messages);
+
+  socket.on('sendMessage', data => {
+    messages.push(data);
+
+    socket.broadcast.emit('receivedMessage', data);
+  });
+});
+
+server.listen(3000);
